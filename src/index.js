ac189e444674718c0f0415fba7f3b1c51f3df96a
Nelson <nelson.mieszkalski@gmail.com>
2022-03-31 15:37:27 -0400

    Added/Started project factory function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
+// Task Factory
 const Task = (title, details, priority, dueDate, status) => {
-    const getTitle = () => title;
-    const setTitle = (newTitle) => title = newTitle;
+    const getTitle = (() => title)();
+    const setTitle = newTitle => title = newTitle;
 
-    const getDetails = () => details;
-    const setDetails = (newDetails) => details = newDetails;
+    const getDetails = (() => details)();
+    const setDetails = newDetails => details = newDetails;
 
-    const getPriority = () => priority;
-    const setPriority = (newPriority) => priority = newPriority
+    const getPriority = (() => priority)();
+    const setPriority = newPriority => priority = newPriority
 
-    const getDueDate = () => dueDate;
-    const setDueDate = (newDueDate) => dueDate = newDueDate;
+    const getDueDate = (() => dueDate)();
+    const setDueDate = newDueDate => dueDate = newDueDate;
 
-    const getStatus = () => status;
-    const setStatus = (newStatus) => status = newStatus;
+    const getStatus = (() => status)();
+    const setStatus = newStatus  => status = newStatus;
 
     return {
         getTitle, setTitle, 
@@ -22,3 +23,22 @@ const Task = (title, details, priority, dueDate, status) => {
         getStatus, setStatus
     }
 };
+
+
+const Project = (title) => {
+    const getProjectTitle = (() => title)();
+    const setProjectTitle = (newTitle) => title = newTitle;
+
+    const tasks = []
+
+    const getTasks = (() => tasks)();
+    const setTasks = newTasks => tasks = newTasks
+    
+    const getTask = {}
+
+    const containsTask = {}
+
+    const addTask = {}
+
+    const deleteTask = {}
+}
