9a0129510047483e24bdc8fd8b06c6de55c96b92
Nelson <nelson.mieszkalski@gmail.com>
2022-04-09 18:27:22 -0400

    Added modal behavior

--- src/user-interface.js
+++ src/user-interface.js
@@ -5,4 +5,44 @@ const toDoList = ToDo()
 
 const defaultProject = Project('Default Project')
 
-console.log(defaultProject)
+
+
+// Modal Actions
+
+const addBookButtons = document.querySelectorAll("[data-modal-target]");
+const closeBookButtons = document.querySelectorAll("[data-close-button]");
+const submitButton = document.querySelector('.add-edit-task');
+const overlay = document.getElementById("overlay");
+
+
+addBookButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const form = document.querySelector(button.dataset.modalTarget);
+        openForm(form);
+    })
+}) 
+
+closeBookButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const form = button.closest(".form")
+        closeForm(form);
+    })
+})
+
+function openForm(form) {
+    if (form == null) return
+    form.classList.add("active");
+    overlay.classList.add("active");
+}
+
+function closeForm(form) {
+    if (form == null) return
+    form.classList.remove("active");
+    overlay.classList.remove("active");
+}
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    const formCard = document.getElementById("form-card");
+    formCard.reset();
+})
